488856022ac8240e5f7ed99a396b17257b521282
Bart Smeets <bart.smeets.qsp@kuleuven.be>
2026-07-27 18:41:55 +0200

    main and doc

--- main.py
+++ main.py
@@ -13,33 +13,42 @@ from PySide6.QtWidgets import (
 )
 
 from fcs_control.devices import get_device_manager
-from fcs_control.gui import LogPanel, MaterialPanel, MenuBar, VoltagePanel
+from fcs_control.gui import DelayPanel, LogPanel, MaterialPanel, MenuBar, VoltagePanel
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Page Settings
         self.setWindowTitle("FCS: Free Cluster Setup")
-
         layout = QHBoxLayout()
+
+        # Panels
+        ## Log Panel
         layout.addWidget(LogPanel(self))
+        self.device_manager = get_device_manager()  # Load device manager after logpanel to add to log
+
         layout.addWidget(VoltagePanel(self))
         layout.addWidget(MaterialPanel(self))
+        layout.addWidget(DelayPanel(self))
 
+        # Cobined Panels as Widget
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
-        
-        self.device_manager = get_device_manager()
-        
+
+        # Menu Bar
         self.menu_bar = MenuBar(self.device_manager, self)
         self.setMenuBar(self.menu_bar)
 
 
-app = QApplication([])
+# Run Application
+def main():
+    app = QApplication([])
+    window = MainWindow()
+    window.showMaximized()
+    sys.exit(app.exec())
 
-window = MainWindow()
-window.showMaximized()
-
-sys.exit(app.exec())
+if __name__ == "__main__":
+    main()
