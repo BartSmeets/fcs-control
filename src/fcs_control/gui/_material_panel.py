23e85f3112c5b88c6a612a1a214a3e0a12dfa9f4
Bart Smeets <bart.smeets.qsp@kuleuven.be>
2026-07-31 15:52:10 +0200

    Materials docstring

--- src/fcs_control/gui/_material_panel.py
+++ src/fcs_control/gui/_material_panel.py
@@ -1,3 +1,9 @@
+"""
+Builds MaterialPanel for material selection 
+
+(which is used in logging)
+
+"""
 from PySide6.QtWidgets import (
     QComboBox,
     QGroupBox,
@@ -7,6 +13,7 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
 )
 
+# Elements to choose from
 ELEMENTS = [
     'H2', 'D2', 'He',
     'C', 'C60', 'CO2', 'N2', 'O',
@@ -15,30 +22,47 @@ ELEMENTS = [
     'Nb', 'Rh', 'Pd', 'Ag'
 ]
 
+
 class MaterialPanel(QGroupBox):
+    """
+    Build the material panel
+
+    Intially combines two option
+    But can be extended, see `add_combobox` and `remove_combobox`
+
+    """
     def __init__(self, parent=None):
         super().__init__('Material Selection', parent)
 
-        layout = QVBoxLayout(self)
-        self.material_layout = QVBoxLayout()
+        # Layout
+        layout = QVBoxLayout(self)  # Total layout
+        self.material_layout = QVBoxLayout()    # Materials selection (no button)
         layout.addLayout(self.material_layout)
 
         self.elements = []
 
+        # Intial Comboboxes
         self.add_combobox()
         self.add_combobox()
 
+        # Add combox button
         add_btn = QPushButton("Add Element")
         add_btn.clicked.connect(self.add_combobox)
         layout.addWidget(add_btn)
         layout.addStretch()
 
     def add_combobox(self):
+        """
+        Add a Combobox widget to the MaterialPanel
+
+        """
         row = QHBoxLayout()
 
+        # Label
         label = QLabel(f"Element {len(self.elements) + 1}:")
         row.addWidget(label)
 
+        # Combobox
         combo = QComboBox()
         combo.setEditable(True)
         combo.addItems(ELEMENTS)
@@ -49,10 +73,14 @@ class MaterialPanel(QGroupBox):
         remove_btn.clicked.connect(lambda: self.remove_combobox(combo, row))
         row.addWidget(remove_btn)
         
-        self.elements.append(combo)
-        self.material_layout.addLayout(row)
+        self.elements.append(combo) # Stores the information (individual comboboxes)
+        self.material_layout.addLayout(row) # Adds row to material layout
 
     def remove_combobox(self, combo, row):
+        """
+        Remove a combobox by removing its row
+
+        """
         self.elements.remove(combo)
 
         while row.count():
@@ -61,6 +89,13 @@ class MaterialPanel(QGroupBox):
                 item.widget().deleteLater()
 
     def get_settings(self):
+        """
+        Returns
+        -------
+        list[str]
+            List of box contents (material selection)
+            
+        """
         return [
             box.currentText()
             for box in self.elements
